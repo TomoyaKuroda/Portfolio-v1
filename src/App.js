cad7c5c499cb1f577e73ed0794fa524cf8939d05
Tomoya <200378857@student.georgianc.on.ca>
2020-02-05 21:38:34 -0500

    Add animation on project page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import gsap from "gsap";
 
 import Header from "./components/Header";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import anime from "animejs";
 import { ContextProvider } from "./contexts/context";
 import useMousePosition from "./hooks/useMousePosition";
 import codeImg from "./assets/gradient.jpg";
@@ -99,78 +98,10 @@ function Home() {
       0.15,
       "Start"
     ).from(contentP, 1, { y: 20, opacity: 0, ease: "power3.out" }, 1.4);
-    // let morphing = anime({
-    //   targets: ".polymorph",
-    //   points: [{ value: "215,110 0,110 0,0 47.7,0 67,76 " }],
-    //   easing: "easeOutQuad",
-    //   duration: 2000,
-    //   loop: false
-    // });
-    // let tl = anime.timeline({
-    //   easing: "easeInOutQuart",
-    //   duration: 4000
-    // });
-    // tl.add({
-    //   targets: "svg",
-    //   keyframes: [
-    //     { scale: 0.3, rotate: "0deg" },
-    //     { scale: 0.5, rotate: "360deg" }
-    //   ],
-    //   duration: 3000
-    // });
-    // tl.add(
-    //   {
-    //     targets: ["#logo", "feTurbulence", "feDisplacementMap"],
-    //     baseFrequency: 0,
-    //     numOctaves: 2,
-    //     rotate: "300deg",
-    //     scale: 1,
-    //     fill: "#fff",
-    //     fillOpacity: "1",
-    //     strokeWidth: 3,
-    //     stroke: "#fff",
-    //     strokeDasharray: "20,10,5,4,30,10"
-    //   },
-    //   "-=4000"
-    // );
-    // tl.add({
-    //   targets: "svg",
-    //   scale: 7,
-    //   duration: 500
-    // });
-    // tl.add({
-    //   targets: "svg",
-    //   opacity: 0,
-    //   duration: 500
-    // });
-    // tl.add({
-    //   targets: "body",
-    //   backgroundColor: "#FFF",
-    //   duration: 500
-    // });
   }, [tl]);
 
   return (
     <div className="hero" ref={el => (app = el)}>
-      {/* <Masonry className="showcase">
-  {data.allDatoCmsWork.edges.map(({ node: work }) => (
-    <div key={work.id} className="showcase__item">
-      <figure className="card">
-        <Link to={`/works/${work.slug}`} className="card__image">
-          <Img fluid={work.coverImage.fluid} />
-        </Link>
-        <figcaption className="card__caption">
-          <h6 className="card__title">
-            <Link to={`/works/${work.slug}`}>{work.title}</Link>
-          </h6>
-          <div className="card__description">
-            <p>{work.excerpt}</p>
-          </div>
-        </figcaption>
-      </figure>
-    </div>
-  ))}
-</Masonry> */}
       <div className="hero-inner">
         <div className="hero-content">
           <div className="hero-content-inner" ref={el => (content = el)}>
@@ -193,25 +124,8 @@ function Home() {
               Passionate about software development and collaborating with
               people.
             </p>
-            {/* <div className="btn-row">
-              <button className="explore-button" onClick={props.handleMenu}>
-                Explore
-                <div className="arrow-icon">
-                  <img src={arrow} alt="row" />
-                </div>
-              </button>
-            </div> */}
           </div>
-          {/* <div className="hero-images">
-        <div ref={el => images = el}  className="hero-images-inner">
-          <div className="hero-image first">
-            <img src={firstImg} alt="laptop" />
-          </div>
-          <div className="hero-image second">
-            <img src={secondImg} alt="laptop" />
-          </div>
-        </div>
-      </div> */}
+
           <div className="wrap">
             <div className={"cube"}>
               <img src={codeImg} alt="" style={first} />
@@ -223,48 +137,6 @@ function Home() {
             </div>
           </div>
         </div>
-
-        {/* <svg viewBox="0 0 248 248.667" id="turbulence">
-          <filter id="displacementFilter">
-            <feTurbulence
-              type="turbulence"
-              baseFrequency="0.05"
-              numOctaves="3"
-              result="turbulence"
-            />
-            <feDisplacementMap
-              in2="turbulence"
-              in="SourceGraphic"
-              scale="50"
-              xChannelSelector="R"
-              yChannelSelector="G"
-            />
-          </filter>
-          <path
-            id="logo"
-            fill="#00AE7B"
-            d="M124,86.421c21.006,0,38.095,17.089,38.095,38.095S145.006,162.612,124,162.612
-        s-38.095-17.089-38.095-38.095S102.994,86.421,124,86.421 M124,57.85c-36.819,0-66.667,29.848-66.667,66.667
-        S87.181,191.183,124,191.183s66.667-29.848,66.667-66.667S160.819,57.85,124,57.85L124,57.85z"
-          />
-        </svg> */}
-        {/* <svg viewBox="0 0 215 110">
-          <polygon
-            id="first"
-            className="polymorph"
-            points="215,110 0,110 0,0 47.7,0 215,0 "
-          />
-          {/* <polygon
-            id="second"
-            className="polymorph"
-            points="215,110 0,110 0,0 47.7,0 67,76 "
-          /> */}
-        {/* </svg>  */}
-        {/* <h5>
-          The <b>HAMBRG</b>, is a creative, engineer driven, global agency
-          working on advancing the software, advertising and design communities
-          to new heights.
-        </h5> */}
       </div>
     </div>
   );
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,78 +1,155 @@
-import React from "react";
+import React, { useEffect } from "react";
+import anime from "animejs";
 
 import assistonImg from "../assets/assiston.png";
 import assistonLaptopImg from "../assets/assistonDesktop.png";
 import archemistMobile from "../assets/archemist-mobile.png";
 import archemistLaptop from "../assets/archemist-laptop.png";
+import { useRef } from "react";
 
 function Projects() {
+  // let title = useRef(null);
+  let title = [];
+  let text = "PROJECTS";
+  text = text.split("");
+  text.forEach(element => {
+    title.push(element);
+  });
+  useEffect(() => {
+    anime
+      .timeline({ loop: false })
+      .add(
+        {
+          targets: ".box .letters",
+          scale: [4, 1],
+          opacity: [0, 1],
+          duration: 900,
+          easing: "easeInOutExpo",
+          delay: anime.stagger(100)
+        },
+        "+=500"
+      )
+      .add(
+        {
+          targets: ".box .line",
+          scaleX: [0, 1],
+          duration: 1400,
+          easing: "easeInOutExpo",
+          delay: anime.stagger(400)
+        },
+        "-=700"
+      );
+  });
   return (
-    <div className="flip-wrapper">
-      <div className="flip-card">
-        <div className="flip-card-inner">
-          <div className="flip-card-front">
-            <img src={assistonImg} alt="" />
-          </div>
-          <div className="flip-card-back">
-            <div className="back-header">
-              <a
-                href="http://assiston-toronto.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={assistonLaptopImg} alt="" />
-              </a>
+    <>
+      <div className="box">
+        <div className="line"></div>
+        <div className="title" ref={el => (title = el)}>
+          {title.map(element => (
+            <span className="letters">{element}</span>
+          ))}
+        </div>
+        <div className="line"></div>
+      </div>
+      <div className="flip-wrapper">
+        <div className="flip-card">
+          <div className="flip-card-inner">
+            <div className="flip-card-front">
+              <img src={assistonImg} alt="" />
             </div>
-            <div className="back-footer">
-              <h2>assist on</h2>
-              <p>
-                I collaborated with a web designer and developed the WordPress
-                website for a study abroad agency.
-              </p>
-              <a
-                href="http://assiston-toronto.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Link
-              </a>
+            <div className="flip-card-back">
+              <div className="back-header">
+                <a
+                  href="http://assiston-toronto.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={assistonLaptopImg} alt="" />
+                </a>
+              </div>
+              <div className="back-footer">
+                <h2>assist on</h2>
+                <p>
+                  I collaborated with a web designer and developed the WordPress
+                  website for a study abroad agency.
+                </p>
+                <a
+                  href="http://assiston-toronto.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Link
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="flip-card">
-        <div className="flip-card-inner">
-          <div className="flip-card-front">
-            <img src={archemistMobile} alt="" />
-          </div>
-          <div className="flip-card-back">
-            <div className="back-header">
-              <a
-                href="https://archemist.org/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={archemistLaptop} alt="" />
-              </a>
+        <div className="flip-card">
+          <div className="flip-card-inner">
+            <div className="flip-card-front">
+              <img src={archemistMobile} alt="" />
             </div>
-            <div className="back-footer">
-              <h2>ARchemisT</h2>
-              <p>
-                I built the website with Figma provided by a designer to get
-                people interested in the crowdfunding project.
-              </p>
-              <a
-                href="https://archemist.org/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Link
-              </a>
+            <div className="flip-card-back">
+              <div className="back-header">
+                <a
+                  href="https://archemist.org/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={archemistLaptop} alt="" />
+                </a>
+              </div>
+              <div className="back-footer">
+                <h2>ARchemisT</h2>
+                <p>
+                  I built the website with Figma provided by a designer to get
+                  people interested in the crowdfunding project.
+                </p>
+                <a
+                  href="https://archemist.org/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Link
+                </a>
+              </div>
             </div>
           </div>
         </div>
+        {/* <div className="flip-card">
+          <div className="flip-card-inner">
+            <div className="flip-card-front">
+              <img src={archemistMobile} alt="" />
+            </div>
+            <div className="flip-card-back">
+              <div className="back-header">
+                <a
+                  href="https://archemist.org/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={archemistLaptop} alt="" />
+                </a>
+              </div>
+              <div className="back-footer">
+                <h2>ARchemisT</h2>
+                <p>
+                  I built the website with Figma provided by a designer to get
+                  people interested in the crowdfunding project.
+                </p>
+                <a
+                  href="https://archemist.org/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Link
+                </a>
+              </div>
+            </div>
+          </div>
+        </div> */}
       </div>
-    </div>
+    </>
   );
 }
 
